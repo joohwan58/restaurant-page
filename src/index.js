3b64df98c78413c4e673d7c29ae26b9be4f31e2a
Kim Joohwan <kim.joohwan58@gmail.com>
2023-07-02 13:49:10 +0900

    partial progress on menu

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import { footer } from "./footer.js";
 import { homepage } from "./homepage.js";
 
 const body = document.querySelector('body');
-body.appendChild(header);
+// body.appendChild(header);
 
-const mainContent = document.createElement('div');
-mainContent.classList.add('main-content');
-mainContent.appendChild(homepage);
+body.insertBefore(header, body.firstChild)
 
-body.appendChild(mainContent);
+// const mainContent = document.createElement('div');
+// mainContent.classList.add('main-content');
+// mainContent.appendChild(homepage);
+
+// body.appendChild(mainContent);
 
 body.appendChild(footer);
